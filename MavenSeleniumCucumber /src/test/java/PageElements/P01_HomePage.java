8c333fc596bafc80da2a75a598e04e416174d42e
Mark Agaibi <103111964+MarkAgaibi@users.noreply.github.com>
2023-03-14 00:18:24 +0200

    Update P01_HomePage.java

--- MavenSeleniumCucumber /src/test/java/PageElements/P01_HomePage.java
+++ MavenSeleniumCucumber /src/test/java/PageElements/P01_HomePage.java
@@ -15,71 +15,23 @@ public class P01_HomePage {
     }
 
 
-    @FindBy(className = "ico-register")
-    public WebElement RegisterationTab;
+    @FindBy(className = "country-current")
+    public WebElement CountryTab;
 
-    @FindBy(className = "ico-login")
-    public WebElement loginTab;
+    @FindBy(id = "translation-btn")
+    public WebElement LanguageButton;
 
-    @FindBy(className = "ico-account")
-    public WebElement MyAccountTab;
-
-    @FindBy(id = "customerCurrency")
-    public WebElement MainCurrency;
-
-    @FindBy(xpath = "//div[@class=\"prices\"] //span")
-    public WebElement ProductCurrency;
-
-    @FindBy(id = "small-searchterms")
-    public WebElement Search;
-
-    @FindBy(css = "button[type=\"submit\"]")
-    public WebElement SearchButton;
-
-    @FindBy(id = "q")
-    public WebElement SearchTextKeyword;
-
-    @FindBy(xpath = "//div [@class=\"header-menu\"]  //ul[@class]")
-    public WebElement HeaderMenu;
-
-    @FindBy(xpath = "//a[@href=\"/computers\"]")
-    public WebElement ComputersMenu;
-
-    @FindBy(xpath = "//a[@href=\"/software\"]")
-    public WebElement SubMenu;
-
-    @FindBy(xpath = "//*[@id=\"nivo-slider\"]/a[1]")
-    public WebElement NokiaImage;
-
-    @FindBy(xpath = "//div[6]/div[3]/div/div/div/div/div[1]/div[2]/a[1]")
-    public WebElement NokiaSlide;
-
-    @FindBy(xpath = "//div[6]/div[3]/div/div/div/div/div[1]/div[2]/a[2]")
-    public WebElement IphoneSlide;
-
-    @FindBy(xpath = "//*[@id=\"nivo-slider\"]/a[2]")
-    public WebElement IphoneImage;
-
-    @FindBy(linkText = "Facebook")
-    public WebElement Facebook;
-
-    @FindBy(linkText = "Twitter")
-    public WebElement Twitter;
-
-    @FindBy(linkText = "RSS")
-    public WebElement RSS;
-
-    @FindBy(linkText = "YouTube")
-    public WebElement Youtube;
-
-    @FindBy(xpath = "//div[@class=\"item-box\"][2]//div[@class=\"product-item\"]//div[@class=\"picture\"]")
-    public WebElement macbookPicture;
-
-    @FindBy(xpath = "//div[@id=\"bar-notification\"]//div")
-    public WebElement SuccessNotification;
-
-    @FindBy(xpath = "//div[@id=\"bar-notification\"]//p")
-    public WebElement SuccessNotificationMessage;
-
+    @FindBy(id = "currency-lite")
+    public WebElement LitePackageEnglishTitle;
+    
+    @FindBy(id = "currency-classic")
+    public WebElement ClassicPackageEnglishTitle;
+    
+     @FindBy(id = "currency-premium")
+    public WebElement PremiumPackageEnglishTitle;
+    
+    @FindBy(id = "currency-premium")
+    public WebElement PremiumPackageEnglishTitle;
+    
 
 }
